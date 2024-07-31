e392d9a50e67f2be8b38435a3643f38dbc96ec9b
Joan Alvarez <joanquique@gmail.com>
2024-07-30 18:00:34 -0600

    U/api-mercadopago

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
+// server.js
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const paymentRoutes = require('./routes/payments');
+
+// Middleware
+app.use(bodyParser.json());
 
 // Servir archivos estáticos desde la raíz
 app.use(express.static(path.join(__dirname, '..')));
@@ -10,6 +16,9 @@ app.use(express.static(path.join(__dirname, '..')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
+
+// Rutas de pagos
+app.use('/api', paymentRoutes);
 
 // Iniciar el servidor
 app.listen(port, () => {

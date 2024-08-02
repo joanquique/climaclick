46ca4c55fd714ad81c6cdc59c0fedf3aff4bd7ce
Joan Alvarez <joanquique@gmail.com>
2024-08-01 18:01:33 -0600

    U/api-mercadopago

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,45 @@
-// server.js
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-const paymentRoutes = require('./routes/payments');
+
+const mercadopago = require('mercadopago');
+const { MercadoPagoConfig, Preference } = mercadopago;
+
+const client = new MercadoPagoConfig({
+  accessToken: 'TEST-3105584100759399-073019-82f63418d8f4c2fc5172aabe0ebdbb4a-1855750645'
+});
+
+const preference = new Preference(client);
+
+preference.create({
+  body: {
+    items: [
+      {
+        title: 'Mi producto',
+        quantity: 1,
+        unit_price: 85,
+        currency_id: 'MXN' // Add currency ID
+      }
+    ],
+    payer: {
+      email: 'payer@example.com' // Add payer email
+    }
+  }
+})
+.then((response) => {
+  console.log(response);
+  if (response.body) {
+    const preferenceId = response.body.id;
+    console.log(`Preferencia creada con ID: ${preferenceId}`);
+  } else {
+    console.log('Error creating preference:', response);
+  }
+})
+.catch((error) => {
+  console.log('Error creating preference:', error);
+});
 
 // Middleware
 app.use(bodyParser.json());
@@ -16,9 +51,6 @@ app.use(express.static(path.join(__dirname, '..')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
-
-// Rutas de pagos
-app.use('/api', paymentRoutes);
 
 // Iniciar el servidor
 app.listen(port, () => {
